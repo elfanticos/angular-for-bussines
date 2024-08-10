9b0d8683d0dffd8d120d8a54698209a82e8413c6
jmeza <jhonatan@smiledu.com>
2024-08-11 00:47:55 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MyCheckboxComponent} from "./my-checkbox/my-checkbox.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyCheckboxComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'checkbox-standalone';
+  isChecked= true;
+  text= "checkbox con texto cambiado";
+  isDisabled = true;
 }
